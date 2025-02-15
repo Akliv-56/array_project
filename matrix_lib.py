cd0ffd89f02439e7ed82530b85d3e0aebf1c55f3
Артем <klivnaka11@gmail.com>
2025-02-15 13:07:37 -0500

    Добавил функцию для вывода верхнего треугольника матрицы

--- matrix_lib.py
+++ matrix_lib.py
@@ -1,8 +1,10 @@
 def upper_triangle(matrix:list[list[int or float]]):
-    #i  0  1  2 j
-    #0 [1, 2, 3],
-    #1 [4, 5, 6],
-    #2 [7, 8, 9]
+    #i  0  1  2  3  4 j
+    #0 [1, 2, 3, 4, 5],
+    #1 [4, 5, 6, 7, 8],
+    #2 [7, 8, 9, 1, 2],
+    #3 [4, 5, 6, 7, 8],
+    #4 [5, 6, 7, 8, 9]
     for i in range(0,len(matrix),1):
         for j in range(i,len(matrix[i]),1):
             print(matrix[i][j], end=" ")
@@ -15,3 +17,15 @@ def lower_triangle(matrix:list[list[int or float]]):
             print(matrix[i][j],end=" ")
         print()
 lower_triangle([[1,2,3],[4,5,6],[7,8,9]])
+
+def top_triangle(matrix:list[list[int or float]]):
+    for j in range(0,len(matrix[0]),1):
+        if j > len(matrix)/2:
+            for i in range(0,len(matrix)-j,1):
+                print(matrix[i][j],end=" ")
+        else:
+            for i in range(0,j + 1,1):
+                print(matrix[i][j],end=" ")
+        print()
+top_triangle([[1,2,3,4,5],[4,5,6,7,8],[7,8,9,1,2],[4,5,6,7,8],[5,6,7,8,9]])
+
