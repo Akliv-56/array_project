f4f5b88f79fe77c25427d320b9fbbdcc07f94226
Артем <klivnaka11@gmail.com>
2025-02-15 12:42:41 -0500

    Создал функцию для вывода левого треугольника матрицы

--- matrix_lib.py
+++ matrix_lib.py
@@ -15,3 +15,21 @@ def lower_triangle(matrix:list[list[int or float]]):
             print(matrix[i][j],end=" ")
         print()
 lower_triangle([[1,2,3],[4,5,6],[7,8,9]])
+
+def left_triangle(matrix:list[list[int or float]]):
+    #i  0  1  2  3  4 j
+    #0 [1, 2, 3, 4, 5],
+    #1 [4, 5, 6, 7, 8],
+    #2 [7, 8, 9, 1, 2],
+    #3 [4, 5, 6, 7, 8],
+    #4 [5, 6, 7, 8, 9]
+    counter = 0
+    for i in range(0,len(matrix),1):
+        if i >= len(matrix)/2:
+            counter += 2
+        for j in range(0,i+1-counter,1):
+            print(matrix[i][j],end=" ")
+        print()
+    
+left_triangle([[1,2,3,4,5],[4,5,6,7,8],[7,8,9,1,2],[4,5,6,7,8],[5,6,7,8,9]])
+            
